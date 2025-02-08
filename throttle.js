76c0f02efbdb9cc0f8f6c8d4ff7201b740a6704c
Phani Kanuri <59728264+phanikum4r@users.noreply.github.com>
2025-02-08 02:25:52 -0700

    Update throttle.js

--- throttle.js
+++ throttle.js
@@ -1,4 +1,18 @@
 function throttle(func, wait) {
+  let timeout = null;
+  return function(...args) {
+    if (!timeout) {
+      func.apply(this, args);
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, wait);
+    }
+  };
+}
+
+
+
+function throttle(func, wait) {
   let lastcall = 0;
   return function (...args) {
     const now = Date.now();
